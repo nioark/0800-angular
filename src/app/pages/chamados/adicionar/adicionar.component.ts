db4c02e914eab0949f96e343280111b93e486fd7
nio <nicolasrausch.c@gmail.com>
2024-02-08 17:56:56 -0300

    adicionado observables de chamados e sincronização em tempo real

--- src/app/pages/chamados/adicionar/adicionar.component.ts
+++ src/app/pages/chamados/adicionar/adicionar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FrameNavComponent } from '../../../components/frame-nav/frame-nav.component';
+import PocketBase from 'pocketbase';
 
 @Component({
   selector: 'app-adicionar',
@@ -9,5 +10,28 @@ import { FrameNavComponent } from '../../../components/frame-nav/frame-nav.compo
   styleUrl: './adicionar.component.scss'
 })
 export class AdicionarComponent {
+  @ViewChild('cliente') cliente:ElementRef | undefined;
+  @ViewChild('descricao') descricao:ElementRef | undefined;
 
+  async createChamado(event : Event) {
+    event.preventDefault()
+    const cliente = this.cliente?.nativeElement.value
+    const descricao = this.cliente?.nativeElement.value
+
+    const pb = new PocketBase('http://127.0.0.1:8090');
+
+    const adminData = await pb.admins.authWithPassword('adm@hardtec.srv.br', 'nany88483240');
+
+    // example create data
+    const data = {
+        "user": "5kj10axm80tq671",
+        "messages": [
+            
+        ],
+        "description": cliente, 
+        "title": descricao
+    };
+
+    const record = await pb.collection('Chamado').create(data);
+  }
 }
